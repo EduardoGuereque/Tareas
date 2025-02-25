c8ac2834d1d387bb3d11df9859f168d8cdc1bd3c
Hp <Hp@172.17.216.43>
2025-02-25 09:21:37 -0700

    Unidad 2 ejercicio 21

--- LoginRemastered/src/Ventana.java
+++ LoginRemastered/src/Ventana.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana extends JFrame {
     private JTextField usuario;
@@ -31,6 +33,35 @@ public class Ventana extends JFrame {
         login = new JButton("Iniciar Sesión");
         panel.add(new JLabel()); // una etiqueta invisible q puse para que el boton de iniciar sesion quede debajo mejor alineadp
         panel.add(login);
+        
+        JMenuBar barra = new JMenuBar();
+        
+        JMenu menu1 = new JMenu("Archivo");
+        JMenu menu2 = new JMenu("Ayuda");
+        
+        barra.add(menu1);
+        barra.add(menu2);
+        
+        JMenuItem abrir = new JMenuItem("Abrir");
+        JMenuItem nuevo = new JMenuItem("Nuevo");
+        JMenuItem guardar = new JMenuItem("Guardar");
+        JMenuItem cerrar = new JMenuItem("Cerrar");
+        
+        JMenuItem bienv = new JMenuItem("Bienvenido");
+        JMenuItem centro= new JMenuItem("Centro de ayuda");
+        JMenuItem reportar = new JMenuItem("Reportar errores");
+        
+        
+        menu1.add(nuevo);
+        menu1.add(abrir);
+        menu1.add(guardar);
+        menu1.add(cerrar);
+        
+        menu2.add(bienv);
+        menu2.add(centro);
+        menu2.add(reportar);
+        
+        this.setJMenuBar(barra);
 
         panel0.add(panel);
         add(panel0);
